6f081141d1359959da402428b94048cd902ef732
victor.icoma@etec.sp.gov.br <victor.icoma@etec.sp.gov.br>
2023-10-15 17:26:45 -0300

    Modelo de dominio anemico relacional com ORM

---
+++ HelperStockBeta/HelperStockBeta.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Envio de chave primária para relacionamento.
+        public ICollection<Product> Products { get; set; }
+    }
+}
