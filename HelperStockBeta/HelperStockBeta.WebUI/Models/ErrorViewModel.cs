fe5ca6921eb45917abb24d06e9ab612cb3d5022c
victor.icoma@etec.sp.gov.br <victor.icoma@etec.sp.gov.br>
2023-10-15 17:34:11 -0300

    Nulo projeto aula

--- HelperStockBeta/HelperStockBeta.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HelperStockBeta.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
