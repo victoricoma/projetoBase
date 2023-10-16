7b0cc4d5dc144ac523206bd5c97227e34df7ea44
victoricoma <victor.icoma@etec.sp.gov.br>
2023-10-16 12:24:44 -0300

    Aula1610 Estrutura de Validação e Dominio

---
+++ HelperStockBeta/HelperStockBeta.Domain/Entities/Category.cs
@@ -0,0 +1,44 @@
+﻿using HelperStockBeta.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Domain.Entities
+{
+    public sealed class Category
+    {
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        //Relaçoes de entidades
+        public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+            Name = name;
+        }
+
+        public Category(int id, string name) 
+        {
+            DomainExceptionValidation.When(id < 0,
+                "Identification is positive values!");
+            Id = id;
+            ValidateDomain(name);
+            Name = name;
+        }
+
+        public void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required!");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Name is minimum 3 charecters");
+        }
+    }
+}
---
+++ HelperStockBeta/HelperStockBeta.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Domain.Entities
+{
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        //Relacionamento com categoria
+        public int CategoryId { get; private set; }
+
+        public Category Category { get; private set; }
+
+    }
+}
