ff584f5928541018077269ce5d78ea9d2d097997
victoricoma <victor.icoma@etec.sp.gov.br>
2023-10-30 10:39:26 -0300

    Aula3010 - Infra de dados e repositórios

---
+++ HelperStockBeta/HelperStockBeta.Infra.Data/EntityConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using HelperStockBeta.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Infra.Data.EntityConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+            builder.HasData(
+                new Category(1, "Material de Escrtório"),
+                new Category(2, "Tecnologia e Eletônicos"),
+                new Category(3, "Acessorios e Equipamentos")
+                );
+        }
+    }
+}
