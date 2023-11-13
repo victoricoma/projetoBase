cb462fa9d31f6f86cbd952b98ee54120b6a4cd0c
victoricoma <victor.icoma@etec.sp.gov.br>
2023-11-13 12:21:59 -0300

    Aula1311 Injeção de Dependencias

---
+++ HelperStockBeta/HelperStockBeta.WebUI/Startup.cs
@@ -0,0 +1,21 @@
+﻿using HelperStockBeta.Infra.IoC;
+using Microsoft.Extensions.Configuration;
+namespace HelperStockBeta.WebUI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddInfrastructure(Configuration);
+            services.AddControllersWithViews();
+        }
+
+
+    }
+}
