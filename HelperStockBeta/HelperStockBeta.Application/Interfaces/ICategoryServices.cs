e7646ddb4809f6dc4063ff299d25cf9bc36e2aac
victoricoma <victor.icoma@etec.sp.gov.br>
2023-11-26 23:49:27 -0300

    Planejamento da Aula 2711 DTOs e Configuração do Azure Database.

---
+++ HelperStockBeta/HelperStockBeta.Application/Interfaces/ICategoryServices.cs
@@ -0,0 +1,16 @@
+﻿using HelperStockBeta.Application.DTOs;
+
+namespace HelperStockBeta.Application.Interfaces
+{
+    public interface ICategoryServices
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+
+        Task Add(CategoryDTO categoryDTO);
+
+        Task Update(CategoryDTO categoryDTO);
+
+        Task remove(int? id);
+    }
+}
---
+++ HelperStockBeta/HelperStockBeta.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HelperStockBeta.Application.DTOs;
+using HelperStockBeta.Domain.Entities;
+
+namespace HelperStockBeta.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
